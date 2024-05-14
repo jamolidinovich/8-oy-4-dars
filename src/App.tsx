209323438a048dbca34564887edd63a74d71342f
anazirjonov <anazirjonov10@gmail.com>
2024-05-14 21:49:33 +0500

    pomodoro finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import Pomodoro from "./components/Timer";
-import MusicPlayer from "./components/MusicPlayer";
 function App() {
   return (
     <div>
       <Pomodoro></Pomodoro>
-      {/* <MusicPlayer></MusicPlayer> */}
     </div>
   );
 }
